f9338ea5582a26b234e4a22010920590efd9cbd5
Calle-99 <72162184+Calle-99@users.noreply.github.com>
2023-06-22 23:44:12 +0800

    更新 speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -4,10 +4,10 @@
     clear
 
 echo "请选择要运行的代码序号："
-echo "1. SuperSpeed 三网全面测速"
-echo "2. 仅进行 speedtest 国内三网测速"
-echo "3. 三网回程路由检测V1"
-echo "4. 三网回程路由检测V2"
+echo "1. backtrace三网回程路由检测"
+echo "2. mtr三网回程路由检测"
+echo "3. SuperSpeed 三网全面测速"
+echo "4. 仅进行 speedtest 国内三网测速"
 echo "5. 流媒体测试"
 echo "6. 国内外网络测速+流媒体测试"
 
@@ -15,16 +15,16 @@ read choice
 
 case $choice in
     1)
-        bash <(curl -Lso- https://git.io/superspeed_uxh)
-        ;;
-    2)
-        bash <(wget -qO- https://down.vpsaff.net/linux/speedtest/superbench.sh) --speed
-        ;;
-    3)
         curl https://raw.githubusercontent.com/zhanghanyun/backtrace/main/install.sh -sSf | sh
         ;;
-    4)
+    2)
         curl https://raw.githubusercontent.com/zhucaidan/mtr_trace/main/mtr_trace.sh|bash
+        ;;
+    3)
+        bash <(curl -Lso- https://git.io/superspeed_uxh)
+        ;;
+    4)
+        bash <(wget -qO- https://down.vpsaff.net/linux/speedtest/superbench.sh) --speed
         ;;
     5)
         bash <(wget -qO- https://down.vpsaff.net/linux/speedtest/superbench.sh) -m
